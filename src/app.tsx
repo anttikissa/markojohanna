0613aa7fdc706d4f96ec047faff5f2c677bb13c5
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2024-09-26 12:20:35 +0300

    Keyboard plays too

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,28 @@
 import './app.css'
+import { useKeyDownEvent } from '@solid-primitives/keyboard'
+import { createEffect } from 'solid-js'
 
 export default function App() {
 	function play() {
 		console.log('Play!')
 	}
-	
+
+	let keyDownEvent = useKeyDownEvent()
+
+	createEffect(() => {
+		let ev = keyDownEvent()
+		if (ev) {
+			play()
+		}
+	})
+
 	return (
 		<main>
 			<div class="player">
-				<button onClick={play}>Play</button>
+				<p>
+					Press any key to
+					<button onClick={play}>Play</button>
+				</p>
 			</div>
 		</main>
 	)
